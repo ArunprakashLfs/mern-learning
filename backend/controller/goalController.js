3398d1bdf625b1de0e303d787e5430c46ccfb92c
222003 <arunprakash7871@gmail.com>
2024-02-28 16:06:37 +0530

    initial backend created goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,33 +1,53 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../modal/goalModal");
 //@desc getGoals
 //route GET/api/goals
 //@access private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get Goals" });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 //@desc setGoals
 //route SET/api/goals
 //@access private
 const setGoals = asyncHandler(async (req, res) => {
-  if (!req.body.table) {
+  if (!req.body.text) {
     res.status(400);
     throw new Error("please add the text field");
   }
-  res.status(200).json({ message: "setGoals" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
 });
 
 //@desc updateGoals
 //route PUT/api/goals/:id
 //@access private
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update Goals ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 //@desc deleteGoals
 //route DELETE/api/goals/:id
 //@access private
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete Goals ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
+  await goal.deleteOne();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getGoals, setGoals, updateGoals, deleteGoals };
---
+++ backend/modal/goalModal.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "please add a text Value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Goal", goalSchema);
